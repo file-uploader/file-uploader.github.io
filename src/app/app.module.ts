5c74bc32b50f3eefd506b6cb0b28f8a633f58313
web-portfolio-angular <atanas.ivanov1808@gmail.com>
2020-11-29 22:24:16 +0200

    upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireStorageModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCR1RKN9XZPSUjUysMsJrnH91DeKBrtgxc",
+    authDomain: "file-uploader-e1b72.firebaseapp.com",
+    databaseURL: "https://file-uploader-e1b72.firebaseio.com",
+    projectId: "file-uploader-e1b72",
+    storageBucket: "file-uploader-e1b72.appspot.com",
+    messagingSenderId: "362566717925",
+    appId: "1:362566717925:web:17e8cb6efe6b1d9ef09f0c",
+    measurementId: "G-QC84H0ZZ6N"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
